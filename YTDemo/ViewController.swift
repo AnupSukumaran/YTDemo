5e381f9d49fbcb6b859ced68104d87829343ea6a
Abraham VG <webindiasolutions@abrahams-mac-mini.local>
2018-06-13 18:08:59 +0530

    add url

--- YTDemo/ViewController.swift
+++ YTDemo/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-
+//https://www.appcoda.com/youtube-api-ios-tutorial/
     @IBOutlet weak var tblVideos: UITableView!
     
     @IBOutlet weak var segDisplayedContent: UISegmentedControl!
